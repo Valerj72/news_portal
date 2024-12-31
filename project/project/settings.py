03970b129625238c59dba40324d01e761e05ed66
Sony <em5450353@gmail.com>
2024-12-31 22:00:52 +0300

    Commit eight

--- project/project/settings.py
+++ project/project/settings.py
@@ -168,3 +168,10 @@ MANAGERS = (
 ADMINS = (
     ('anton', 'anton@yandex.ru'),
 )
+
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
